7cef6680617500026a145844cd0f1ab1f83e0699
Yehor Pliasov <fenix4088@gmail.com>
2021-02-23 11:52:34 +0100

    02 Navigation menu ready

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,22 +1,36 @@
 import React from "react";
-import n from "./Navigation.scss"
+import "./Navigation.scss"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {
+    faFacebook,
+    faFacebookF,
+    faGithub,
+    faLinkedin,
+    faLinkedinIn,
+    faTelegram,
+    faVk
+} from "@fortawesome/free-brands-svg-icons";
+import gbFlag from "../../assets/img/header/uk.png"
+import ruFlag from "../../assets/img/header/ru.png"
 
 export const Navigation = () => {
     return (
-        <div className="n.fixed-wrapper">
-            <nav className="n.header__navigation">
+        <div className="fixed-wrapper">
+
+            <nav className="header__navigation">
                 <div className="header__navigation-logo">
                         <span className="title-line">
                             <a href="./index.html">YP</a>
                         </span>
 
                 </div>
-                <div className="header__burger-wrap">
+
+{/*                <div className="header__burger-wrap">
                     <label className="header__burger-label">
                         <input type="checkbox" className="header__burger-checkbox"/>
                             <span className="header__burger"></span>
                     </label>
-                </div>
+                </div>*/}
 
                 <div className="header__navigation-block">
                     <ul className="header__navigation-list">
@@ -34,28 +48,28 @@ export const Navigation = () => {
 
                     <div className="intouch-block intouch-block--mobile">
                         <a href="https://github.com/Fenix4088?tab=repositories" target="_blank">
-                            <i className="fab fa-github fa-3x"></i>
+                            <FontAwesomeIcon icon={faGithub} size={"3x"} className={"fa-github"}/>
                         </a>
 
                         <a href="#">
-                            <i className="fab fa-telegram fa-3x"></i>
-                        </a>
-
-                        <a href="https://vk.com/id13266823" target="_blank">
-                            <i className="fab fa-vk fa-vk--modify"></i>
+                            <FontAwesomeIcon icon={faTelegram} size={"3x"} className={"fa-telegram"}/>
                         </a>
 
                         <a href="https://www.facebook.com/profile.php?id=100013553615468" target="_blank">
-                            <i className="fab fa-facebook fa-3x"></i>
+                            <FontAwesomeIcon icon={faFacebook} size={"3x"} className={"fa-facebook"}/>
+                        </a>
+
+                        <a href="https://www.linkedin.com/in/yehor-pliasov-5776981a2/" target="_blank">
+                            <FontAwesomeIcon icon={faLinkedin} size={"3x"} className={"fa-linkedin-in"}/>
                         </a>
                     </div>
 
                     <div className="header__navigation-language header__navigation-language--mobile">
                         <a href="./indexRU.html">
-                            <img src="./img/header/ru.png" alt="Russian flag"/>
+                            <img src={ruFlag} alt="Russian flag"/>
                         </a>
                         <a href="./index.html">
-                            <img src="./img/header/uk.png" alt="England flag"/>
+                            <img src={gbFlag} alt="England flag"/>
                         </a>
                     </div>
 
@@ -65,30 +79,29 @@ export const Navigation = () => {
 
                 </div>
 
-
                 <div className="header__navigation-language">
-                    <a href="./indexRU.html">
-                        <img src="./img/header/ru.png" alt="Russian flag"/>
+                    <a href="#">
+                        <img src={gbFlag} alt="Russian flag"/>
                     </a>
-                    <a href="./index.html">
-                        <img src="./img/header/uk.png" alt="England flag"/>
+                    <a href="#">
+                        <img src={ruFlag} alt="England flag"/>
                     </a>
                 </div>
 
                 <div className="header__navigation-icons">
                     <div className="header__navigation-icon linkedin-icon">
                         <a href="https://www.linkedin.com/in/yehor-pliasov-5776981a2/" target="_blank">
-                            <i className="fab fa-linkedin-in fa-2x"></i>
+                            <FontAwesomeIcon icon={faLinkedinIn} size={"2x"} className={"fa-linkedin-in"}/>
                         </a>
                     </div>
                     <div className="header__navigation-icon git-icon">
                         <a href="https://github.com/Fenix4088?tab=repositories" target="_blank">
-                            <i className="fab fa-github fa-2x"></i>
+                            <FontAwesomeIcon icon={faGithub} size={"2x"} className={"fa-linkedin-in"}/>
                         </a>
                     </div>
                     <div className="header__navigation-icon facebook-icon">
                         <a href="https://www.facebook.com/profile.php?id=100013553615468&ref=bookmarks" target="_blank">
-                            <i className="fab fa-facebook-f fa-2x"></i>
+                            <FontAwesomeIcon icon={faFacebookF} size={"2x"} className={"fa-facebook-f"}/>
                         </a>
                     </div>
                 </div>
